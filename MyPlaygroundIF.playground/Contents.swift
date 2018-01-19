307384687e9e50da63f7a91cf2ed34c0d8348295
Servin <eduardo.servin@gmail.com>
2018-01-19 16:41:57 -0600

    Quitar comentarios

--- MyPlaygroundIF.playground/Contents.swift
+++ MyPlaygroundIF.playground/Contents.swift
@@ -2,38 +2,7 @@
 
 import UIKit
 
-/*var meses = 1...12
 
-for mes in meses {
-   // print(mes)
-}
-
-for mes in meses {
-    
-    
-   /* if mes == 1 {
-        print("enero")
-    }else if mes == 2{
-        print("febrero")
-    }else{
-        print("otro")
-    }*/
-    
-    switch mes {
-    case 1,2,3:
-        print("mes templado")
-    case 4...7:
-        print("mes caluroso")
-    default:
-        print("no hay clima")
-    } 
-}
-
-if true && false {
-    print("true")
-} else if true || false {
-    print("false")
-}*/
 
 let a = 50 % 5
 
